699f296ac4b8b21d16ebe70f5d062f910cc97946
Daria Jung <djj2115cu@gmail.com>
2014-06-18 11:40:27 -0400

    fix pure instance for ziplist

--- Applicative_functors/ziplist.hs
+++ Applicative_functors/ziplist.hs
@@ -6,5 +6,5 @@ instance Functor ZipList where
     fmap f (ZipList xs) = ZipList (map f xs)
  
 instance Applicative ZipList where
-    pure x = ZipList $ [x]
+    pure x = ZipList (repeat x)
     (ZipList gs) <*> (ZipList xs) = ZipList (zipWith ($) gs xs)
