86c7b0d566d4d9cdf0c22423e434e15908a7bb23
Daria Jung <djj2115cu@gmail.com>
2014-06-18 11:04:55 -0400

    implement pure for applicantive instance of ziplist

---
+++ Applicative_functors/ziplist.hs
@@ -0,0 +1,10 @@
+import Control.Applicative hiding (ZipList)
+
+newtype ZipList a = ZipList { getZipList :: [a] }
+
+instance Functor ZipList where
+    fmap f (ZipList xs) = ZipList (map f xs)
+ 
+instance Applicative ZipList where
+    pure x = ZipList $ [x]
+    (ZipList gs) <*> (ZipList xs) = ZipList (zipWith ($) gs xs)
